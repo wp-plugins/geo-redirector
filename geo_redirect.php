bd1a819f721ca3d1fbe27350eb5522c704aba782
AnushkaKR <AnushkaKR@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-07 17:19:50 +0000

    git-svn-id: https://plugins.svn.wordpress.org/geo-redirector/trunk@609225 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ geo_redirect.php
@@ -0,0 +1,241 @@
+<?php
+
+/*
+
+Plugin Name: GEO Redirector
+
+Plugin URI: http://anushkar.com
+
+Description: Redirect pages according to the geo location
+
+Author: Anushka Rajasingha
+
+Author URI: http://anushkar.com
+
+Version: 1.0.0
+
+*/
+
+
+
+
+
+
+
+register_activation_hook( __FILE__, 'geo_redirector_install' );
+
+register_deactivation_hook(__FILE__,'geo_redirector_uninstall');
+
+add_action('wp_head', 'custom_header_function');
+
+if (is_admin()) {
+
+	include "installer.php";
+
+}
+
+
+
+function custom_header_function(){
+
+if (!is_admin()) {
+
+$to = 'http://www.google.com';
+
+//header("Location: " . $to);
+
+	//	exit;
+
+echo '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>';
+
+echo  '<script type="text/javascript" src="http://www.google.com/jsapi"></script>';
+
+echo  '<script src="http://maps.google.com/maps/api/js?sensor=false&v=3&libraries=geometry" type="text/javascript"></script> ';
+
+echo '<script src="'.plugins_url('/js/clientredirect.php', __FILE__).'" type="text/javascript" > </script>';
+
+}
+
+
+
+}
+
+
+
+// create custom plugin settings menu
+
+add_action('admin_menu', 'geo_redirect_menu');
+
+
+
+function geo_redirect_menu() {
+
+
+
+	//create new top-level menu
+
+	add_menu_page('GEO Redirector Plugin Settings', 'GEO Redirector', 'administrator', __FILE__, 'geo_redirect_settings_page',plugins_url('/images/f_redirect.png', __FILE__));
+
+
+
+	//call register settings function
+
+	
+
+}
+
+
+
+
+
+
+
+
+
+function geo_redirect_settings_page() {
+
+?>
+
+<div class="wrap">
+
+<h2>GEO Redirector</h2>
+
+<script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script> 
+
+<script src="<?php echo plugins_url('/js/form_action.php', __FILE__); ?>" type="text/javascript" > </script>
+
+<script type="text/javascript" >
+
+
+
+</script>
+
+<div id="message">
+
+
+
+</div>
+
+<form method="post" onsubmit="geo_redirect_onsubmit(this);return false;" id="geo_submitform" >
+
+ 
+
+     <table class="form-table">
+
+     
+
+        
+
+        <tr valign="top">
+
+        <th scope="row">Street Address Or ZIP Code</th>
+
+        <td width="400px"><input type="text" name="address"  class="regular-text required" title="Address / Physical Location Name"/> <br/><small>After you enter the address or ZIP code please verify the location before it save.</small></td>
+
+        <td width="100px;"> <input type="button" class="button-primary" value="<?php _e('Location Verify') ?>" onclick="get_langlongfromaddress(jQuery('input[name=address]').val());" />  </td>
+
+        <td rowspan="4"><div id="map_canvas" style="display:block;width:100%;height:200px;"></div></td>
+
+        </tr>
+
+         <tr valign="top">
+
+        <th scope="row">Formatted address</th>
+
+        <td><input type="text" disabled="disabled" name="formatted_address" class="regular-text"  id="formatted_address"  /> </td>
+
+        <td ></td>
+
+        </tr>
+
+        <tr valign="top">
+
+        <th scope="row">Latitude</th>
+
+        <td><input type="text" name="latitude" title="Latitude" class="required" /></td>
+
+        <td ></td>
+
+        </tr>
+
+        
+
+        <tr valign="top">
+
+        <th scope="row">Longitude</th>
+
+        <td><input type="text" name="longitude" title="Longitude"  class="required" /></td>
+
+                <td ></td>
+
+        </tr>
+
+        
+
+        <tr valign="top">
+
+        <th scope="row">Radius</th>
+
+        <td><input type="text" name="radius" title="Radius"  class="required" />&nbsp;km</td>
+
+                <td ></td>
+
+        </tr>
+
+         <tr valign="top">
+
+        <th scope="row">URL</th>
+
+        <td><input type="text" name="cur_url" class="regular-text" title="Redirect URL"  /><br/><small>if you leave this field empty, the default value would be <?php echo get_site_url(); ?></small></td>
+
+                <td ></td>
+
+        </tr>
+
+        <tr valign="top">
+
+        <th scope="row">Redirect URL</th>
+
+        <td><input type="text" name="red_url" class="regular-text required" title="Redirect URL"  /><br/><small>Please enter full URL (Ex: http://www.abcdef.com).</small></td>
+
+                <td ></td>
+
+        </tr>
+
+         <tr valign="top">
+
+        <th scope="row">Not in Radius</th>
+
+        <td><input type="checkbox" name="isnot" title="Not In" /></td>
+
+        <td ></td>
+
+        </tr>
+
+       
+
+    </table>
+
+     <input type="hidden" value="" name="hid_id" id="hid_id" />
+
+    <p class="submit">
+
+    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+
+    <input type="button" id="btn_cancel" class="button-primary" value="<?php _e('Cancel') ?>" style="display:none;" onclick="reset_from();" />
+
+    </p>
+
+
+
+</form>
+
+<div id="geo_datadisplay">
+
+<?php include('display.php'); ?>
+
+</div>
+
+</div>
+
+<?php } ?>
---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== Plugin Name ===
+Contributors: AnushkaKR
+Donate link: http://anushkar.com/geo-redirector/
+Tags: GEO, Redirect, GEO Redirector,Redirector,URL
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+This is a WordPress plugin , this plugin will allow you to redirect your URLs according to the visitors geographical location. And you can manage redirect the visitor given radios inside or outside.This plugin was developed by AnushkaKR.
+
+
+== Installation ==
+
+Download the plugin compressed file.
+Unzip geo_redirector.zip file.
+Upload geo_redirector folder to the /wp-content/plugins/ directory.
+Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+What type of techniques use to redirect the URL ? 
+
+google map API.
+
+== Screenshots ==
+
+1. This screen shot describes how to activate the plugin through the 'Plugins' menu in WordPress. '/assets/plugin page.png'
+2. This screen shot describes how to shows up the plugin on left side menus '/assets/menu.png'
+2. This screen shot describes how make changes from plugin setting page '/assets/settingd page.png'
+
+== Changelog ==
+
+= 1.0 =
+* A change since the previous version.
+* Another change.
+
+== Arbitrary section ==
+N/A
+== A brief Markdown Example ==
+
+Ordered list:
+1. Redirect your URLs according to the visitors geographical location
+
+Here's a link to [WordPress](http://wordpress.org/ "Your favorite software") and one to [Markdown's Syntax Documentation][markdown syntax].
+Titles are optional, naturally.
+
+[markdown syntax]: http://daringfireball.net/projects/markdown/syntax
+            "Markdown is what the parser uses to process much of the readme file"
+
+Markdown uses email style notation for blockquotes and I've been told:
+> Asterisks for *emphasis*. Double it up  for **strong**.
+
+`<?php code(); // goes in backticks ?>`
